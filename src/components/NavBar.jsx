346893840cf0750a80496c0e94c88ea104d37826
Jinesh Modi <4jmodi@gmail.com>
2024-10-13 23:42:19 +0530

    Added Resume tab in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,35 +26,36 @@ const NavBar = () => {
             link: 'contact',
         },
     ];
+
     return (
         <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
             <div>
                 <h1 className='text-5xl font-signature ml-2'>Jinesh Modi</h1>
             </div>
             
-            <ul className='hidden md:flex'>
-                {
-                    links.map(
-                        ({ id, link }) => {
-                            return <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200'>
-                                <Link to={link} smooth duration={500}>{link}</Link>
-                            </li>
-                        }
-                    )
-                }
-            </ul>
-
-            <div>
-                
+            <div className='flex items-center'>
+                <ul className='hidden md:flex'>
+                    {
+                        links.map(
+                            ({ id, link }) => {
+                                return <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200'>
+                                    <Link to={link} smooth duration={500}>{link}</Link>
+                                </li>
+                            }
+                        )
+                    }
+                </ul>
+                <a href="/resume.pdf" download className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200'>
+                    {'Resume'}
+                </a>
             </div>
 
-            <a href="/resume.pdf" download className='text-3xl font-signature ml-1 underline text-blue-500 hover:text-blue-700'>
-                {'Click Here -> Resume'}
-            </a>
+           
 
             <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500 hover:text-white md:hidden'>
                 {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
             </div>
+
             {
                 nav && (
                     <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
